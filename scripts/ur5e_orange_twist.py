e0892c33ebf4f92db8d6541607328153ec2b71e1
haggbart <mr.haggbart@gmail.com>
2020-08-04 10:51:06 +0200

    init

---
+++ scripts/ur5e_orange_twist.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from geometry_msgs.msg import Twist
+
+
+MULTIPLIER = 1.0
+SHIFT = 0.0
+SECONDS_PER_MOVE = 0.0010  # doesn't need to be accurate, will accelerate to catch up and smooth its movement
+
+
+NODE_NAME = "ur5e_sin_publisher"
+PUBLISHER = "/scaled_pos_joint_traj_controller/command"
+SUBSCRIBER = "/test_msg"
+
+JOINT_NAMES = ['elbow_joint', 'shoulder_lift_joint', 'shoulder_pan_joint',
+               'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+
+
+# dont edit below pls
+NSECS = SECONDS_PER_MOVE % 1 * 10000000000
+SECS = int(SECONDS_PER_MOVE)
+
+print("secs: " + str(SECS))
+print("nsecs: " + str(NSECS))
+
+
+def get_msg(rotation):
+    msg = JointTrajectory()
+    msg.header.stamp = rospy.Time.now()
+    msg.joint_names = JOINT_NAMES
+    joint_trajectory_point = JointTrajectoryPoint()
+    joint_trajectory_point.positions = [1.57, -1.57, -3.14, -1.57, rotation * MULTIPLIER + SHIFT, 0.0]
+    joint_trajectory_point.time_from_start.secs = SECS
+    joint_trajectory_point.time_from_start.nsecs = NSECS
+    msg.points.append(joint_trajectory_point)
+    return msg
+
+
+def callback_rotation(msg):
+    rotation_value = msg.angular.z
+    msg = get_msg(rotation_value)
+    pub.publish(msg)
+    rospy.loginfo("published rotation: " + str(rotation_value))
+
+
+if __name__ == '__main__':
+    rospy.init_node(NODE_NAME)
+    pub = rospy.Publisher(PUBLISHER, JointTrajectory, queue_size=1)
+    sub = rospy.Subscriber(SUBSCRIBER, Twist, callback_rotation)
+    rospy.loginfo("Initialized.")
+    rospy.spin()
